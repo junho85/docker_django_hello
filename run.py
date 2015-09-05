a9cdd85a80aa855f4bcec268b835af283d19ef43
junho85 <junho85@gmail.com>
2015-09-06 01:20:05 +0900

    minor fix

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ import subprocess
 os.system('docker run -d -p 9999:8000 --name hello junho85/django')
 ip = subprocess.check_output('docker-machine ip default', shell=True).rstrip().decode("utf-8")
 
-port_info = subprocess.check_output('docker port hello', shell=True).decode("utf-8")
+port_info = subprocess.check_output('docker port hello', shell=True).rstrip().decode("utf-8")
 port = port_info.split(":")[1]
 print("http://%s:%s/hello" % (ip, port))
